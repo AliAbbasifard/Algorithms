c7f90043516126db09eb18b5ea249a05d6b80b7b
Abbasifard <aliabbasifard@hotmail.com>
2022-06-21 15:39:18 +0430

    Add [Getting Nodes and Relations]

--- Dynamic Connectivity/Main/Program.cs
+++ Dynamic Connectivity/Main/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyOwnSolution;
+using System;
+using System.Collections.Generic;
 
 namespace Main
 {
@@ -6,7 +8,46 @@ namespace Main
     {
         static void Main(string[] args)
         {
-            
+            IList<int> nodes = new List<int>();
+            IList<(int, int)> relations = new List<(int, int)>();
+
+            int nodeNumber = 0;
+            bool finishRelation = false;
+
+            Console.WriteLine("First enter Nodes. -1 means end.");
+            while (true)
+            {
+                Console.Write($"Node Number: {nodeNumber + 1} Value: ");
+                int nodeValue = int.Parse(Console.ReadLine());
+
+                if (nodeValue.Equals(-1))
+                    break;
+
+                nodes.Add(nodeValue);
+                nodeNumber += 1;
+            }
+
+
+            Console.WriteLine("Now enter Relations.");
+            while (finishRelation is false)
+            {
+                Console.Write("From: ");
+                int from = int.Parse(Console.ReadLine());
+                if (nodes.Contains(from) is false)
+                    break;
+
+                Console.Write("To: ");
+                int to = int.Parse(Console.ReadLine());
+                if (nodes.Contains(to) is false)
+                    break;
+
+                relations.Add((from, to));
+
+                Console.Write("Finished? ");
+                finishRelation = Console.ReadLine().ToLower().Equals("y") ? true : false;
+            }
+
+            //var result = Solution1.Solve(board);
         }
     }
 }
---
+++ Dynamic Connectivity/MyOwnSolution/Solution1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOwnSolution
+{
+    public class Solution1
+    {
+        public static bool Solve(IList<(int, int)> board)
+        {
+            return true;
+        }
+    }
+}
