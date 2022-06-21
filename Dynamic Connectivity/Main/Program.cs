23c8cf206a8edafbbf7ee38d38bda1e9be7dfcab
AliAbbasifard <aliabbasifard2hotmail.com>
2022-06-21 12:32:23 -0700

    First structure of algorithm

--- Dynamic Connectivity/Main/Program.cs
+++ Dynamic Connectivity/Main/Program.cs
@@ -8,46 +8,51 @@ namespace Main
     {
         static void Main(string[] args)
         {
-            IList<int> nodes = new List<int>();
-            IList<(int, int)> relations = new List<(int, int)>();
+            // create board
+            ICollection<int> nodes = new List<int> { 1, 2, 3};
+            ICollection<(int, int, bool)> pathes = new List<(int, int, bool)> { (1, 2, false) };
 
-            int nodeNumber = 0;
-            bool finishRelation = false;
+            //int nodeNumber = 0;
+            //bool finishRelation = false;
 
-            Console.WriteLine("First enter Nodes. -1 means end.");
-            while (true)
-            {
-                Console.Write($"Node Number: {nodeNumber + 1} Value: ");
-                int nodeValue = int.Parse(Console.ReadLine());
+            //Console.WriteLine("First enter Nodes. -1 means end.");
+            //while (true)
+            //{
+            //    Console.Write($"Node Number: {nodeNumber + 1} Value: ");
+            //    int nodeValue = int.Parse(Console.ReadLine());
 
-                if (nodeValue.Equals(-1))
-                    break;
+            //    if (nodeValue.Equals(-1))
+            //        break;
 
-                nodes.Add(nodeValue);
-                nodeNumber += 1;
-            }
+            //    nodes.Add(nodeValue);
+            //    nodeNumber += 1;
+            //}
+
+            //Console.WriteLine("Now enter Relations.");
+            //while (finishRelation is false)
+            //{
+            //    Console.Write("From: ");
+            //    int from = int.Parse(Console.ReadLine());
+            //    if (nodes.Contains(from) is false)
+            //        break;
+
+            //    Console.Write("To: ");
+            //    int to = int.Parse(Console.ReadLine());
+            //    if (nodes.Contains(to) is false)
+            //        break;
+
+            //    relations.Add((from, to));
+
+            //    Console.Write("Finished? ");
+            //    finishRelation = Console.ReadLine().ToLower().Equals("y") ? true : false;
+            //}
+
+            // core
+            //var result = Solution1.Solve(nodes, pathes, int.Parse(args[0]), int.Parse(args[1]));
+            var result = Solution1.Solve(nodes, pathes, 1, 3);
 
 
-            Console.WriteLine("Now enter Relations.");
-            while (finishRelation is false)
-            {
-                Console.Write("From: ");
-                int from = int.Parse(Console.ReadLine());
-                if (nodes.Contains(from) is false)
-                    break;
-
-                Console.Write("To: ");
-                int to = int.Parse(Console.ReadLine());
-                if (nodes.Contains(to) is false)
-                    break;
-
-                relations.Add((from, to));
-
-                Console.Write("Finished? ");
-                finishRelation = Console.ReadLine().ToLower().Equals("y") ? true : false;
-            }
-
-            //var result = Solution1.Solve(board);
+            Console.WriteLine(result);
         }
     }
 }
--- Dynamic Connectivity/MyOwnSolution/Solution1.cs
+++ Dynamic Connectivity/MyOwnSolution/Solution1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,41 @@ namespace MyOwnSolution
 {
     public class Solution1
     {
-        public static bool Solve(IList<(int, int)> board)
+        public static bool Solve(IEnumerable<int> nodes, IEnumerable<(int, int, bool)> pathes, int start, int end)
         {
+            var pathStack = new Stack<(int, int, bool)>();
+
+            // if start or end not in nodes, return false
+            if ((!nodes.Contains(start)) || (!nodes.Contains(end)))
+                return false;
+
+            if (start.Equals(end))
+                return true;
+
+            // if start != end and there is no path in board
+            if (pathes.Count().Equals(0))
+                return false;
+
+            var current = start;
+
+            while (!current.Equals(end))
+            {
+                // find available path from current node
+                var availablePath = pathes.Where(path => (path.Item1.Equals(start) || path.Item2.Equals(start)) && path.Item3.Equals(false));
+                
+                foreach (var path in availablePath)
+                    pathStack.Push(path);
+
+                if (!availablePath.Any() && !pathStack.Any())
+                    return false;
+
+                var choosenPath = pathStack.Pop();
+
+                var next = choosenPath.Item1.Equals(current) ? choosenPath.Item2 : choosenPath.Item1;
+
+                current = next;
+            }
+
             return true;
         }
     }
