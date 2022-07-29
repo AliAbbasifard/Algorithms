1b53dfbda055a108d022e698049494486afa352b
AliAbbasifard <aliabbasifard2hotmail.com>
2022-07-28 20:56:29 -0700

    Selection Sort

---
+++ Sort/Sort/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Sort/Sort/SelectionSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    internal static class SelectionSort
+    {
+        public static IEnumerable<IComparable> Sort(List<IComparable> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                var min = i;
+                for (int j = i+1; j < list.Count; j++)
+                {
+                    if (list[j].CompareTo(list[i]) < 0)
+                        min = j;
+                }
+
+                var x = list[i];
+                list[i] = list[min];
+                list[min] = x;
+            }
+
+            return list;
+        }
+    }
+}
