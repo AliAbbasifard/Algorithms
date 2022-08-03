6ad7fb0b134007692265f2e773a96274f0b2a157
AliAbbasifard <aliabbasifard2hotmail.com>
2022-08-03 10:17:15 -0700

    ShellSort Code

---
+++ Sort/Sort/ShellSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    internal static class ShellSort
+    {
+        public static IEnumerable<IComparable> Sort(IList<IComparable> nums)
+        {
+            int n = nums.Count;
+            int h = 1;
+
+            while (h < n / 3)
+                h = (3 * h) + 1;
+
+            while (h >= 1)
+            {
+                for (int i = h; i < n; i++)
+                {
+                    for (int j = i; j >= h && nums[j].CompareTo(nums[j-h]) < 0; j -= h)
+                    {
+                        var temp = nums[j];
+                        nums[j] = nums[j-h];
+                        nums[j-h] = temp;
+                    }
+                }
+
+                h = h / 3;
+            }
+
+            return nums;
+        }
+    }
+}
