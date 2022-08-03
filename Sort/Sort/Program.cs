70e762025629a7d1ada30c003c51aa00f6950520
AliAbbasifard <aliabbasifard2hotmail.com>
2022-08-03 10:17:44 -0700

    Last Update

--- Sort/Sort/Program.cs
+++ Sort/Sort/Program.cs
@@ -9,27 +9,6 @@ namespace Sort
     {
         static void Main(string[] args)
         {
-            Stopwatch stopwatch = new Stopwatch();
-            var rnd = new Random();
-            var nums = new List<IComparable>();
-            for (int i = 0; i < 10000000; i++)
-                nums.Add(rnd.Next(1, 10000000));
-
-            Console.WriteLine("Size: 10000000");
-            Console.WriteLine("--------------Shell-------------------------------");
-            stopwatch.Start();
-            var result = ShellSort.Sort(nums);
-            stopwatch.Stop();
-            Console.WriteLine($"{stopwatch.ElapsedMilliseconds} ms");
-            Console.WriteLine("-------------Insertion----------------------------------");
-            stopwatch.Reset();
-            stopwatch.Start();
-            var result2 = InsertionSort.Sort(nums);
-            stopwatch.Stop();
-            Console.WriteLine($"{stopwatch.ElapsedMilliseconds} ms");
-            Console.WriteLine("--------------------------------------------------");
-            stopwatch.Reset();
-
         }
     }
 }
