d9c4029facea16e859b43238a6ef8aa3db93733f
AliAbbasifard <aliabbasifard2hotmail.com>
2022-08-04 06:15:38 -0700

    Update Insertion Sort

--- Sort/Sort/InsertionSort.cs
+++ Sort/Sort/InsertionSort.cs
@@ -8,11 +8,11 @@ namespace Sort
 {
     internal static class InsertionSort
     {
-        public static TSource[] ISort<TSource>(this TSource[] array) where TSource : IComparable
+        public static TSource[] ISort<TSource, TKey>(this TSource[] array, Func<TSource, TKey> key) where TKey : IComparable
         {
             for (var i = 0; i < array.Length; i++)
             {
-                for (var j = i; j > 0 && array[j - 1].CompareTo(array[j]) > 0; j--)
+                for (var j = i; j > 0 && key(array[j - 1]).CompareTo(key(array[j])) > 0; j--)
                     (array[j], array[j - 1]) = (array[j - 1], array[j]);
             }
 
