0bf37a62b590647fe5341fce89dd2ac9d374537f
Abbasifard <aliabbasifard@hotmail.com>
2022-08-15 16:09:58 +0430

    -

--- Sort/Sort/Program.cs
+++ Sort/Sort/Program.cs
@@ -9,9 +9,6 @@ namespace Sort
     {
         static void Main(string[] args)
         {
-            var numbers = new int[] { 1, 5, 7, -1, 3, 9, 11, 6, -100};
-
-            MergeSort.Sort(numbers, i => i, 0, numbers.Length - 1);
         }
     }
 }
