76ffb316e0dd0910a5b801aebc0c9af390d54909
Abbasifard <aliabbasifard@hotmail.com>
2022-08-23 15:26:38 +0430

    IComperable Interface

---
+++ Sort/Sort/ComparablePattern.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    //1: ComparablePattern[] x = new ComparablePattern[];
+    //2: Array.Sort(x);
+    public class ComparablePattern : IComparable<Employee>
+    {
+        public int ID;
+        public string EmployeeName;
+        public ComparablePattern(int id, string employeename)
+        {
+            this.ID = id;
+            this.EmployeeName = employeename;
+        }
+
+        public int CompareTo(Employee other)
+        {
+            var t = this;
+            return this.EmployeeName[0].CompareTo(other.EmployeeName[0]);
+        }
+    }
+}
