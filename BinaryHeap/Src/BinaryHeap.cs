b4518eca3e0ce96de531350f9af44916dcfc58d7
Abbasifard <aliabbasifard@hotmail.com>
2022-09-07 19:03:29 +0430

    BinaryHeap DataStructure
    Added to Swim and Insert Methods

---
+++ BinaryHeap/Src/BinaryHeap.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Src
+{
+    public class BinaryHeap<TSource> where TSource : IComparable<TSource>
+    {
+        public readonly IList<TSource> _items;
+        public BinaryHeap(IList<TSource> items)
+        {
+            _items = items.ToList();
+        }
+
+        private void Swim(int k, Func<int, TSource> selector) 
+        {
+            while (k > 0 && selector((k - 1)/2).CompareTo(selector(k)) < 0)
+            {
+                (_items[k], _items[(k - 1) / 2]) = (_items[(k - 1) / 2], _items[k]);
+                k = (k - 1) / 2;
+            }
+        }
+
+        public void Insert(TSource item, Func<int, TSource> selector)
+        {
+            _items.Add(item);
+            Swim(_items.Count() - 1, selector);
+        }
+    }
+}
