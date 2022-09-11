baba0c0afc46528413b7fd9b860fab78a5dd9993
Abbasifard <aliabbasifard@hotmail.com>
2022-09-11 16:37:35 +0430

    HeapSort

--- BinaryHeap/Src/BinaryHeap.cs
+++ BinaryHeap/Src/BinaryHeap.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Src
 {
@@ -14,9 +12,9 @@ namespace Src
             _items = items.ToList();
         }
 
-        private void Swim(int k, Func<int, TSource> selector) 
+        public void Swim(int k, Func<int, TSource> selector)
         {
-            while (k > 0 && selector((k - 1)/2).CompareTo(selector(k)) < 0)
+            while (k > 0 && selector((k - 1) / 2).CompareTo(selector(k)) < 0)
             {
                 (_items[k], _items[(k - 1) / 2]) = (_items[(k - 1) / 2], _items[k]);
                 k = (k - 1) / 2;
@@ -27,6 +25,68 @@ namespace Src
         {
             _items.Add(item);
             Swim(_items.Count() - 1, selector);
+        }
+
+        public void Sink(int k, Func<int, TSource> selector)
+        {
+            while (2 * k + 1 <= _items.Count() - 1)
+            {
+                int j = 2 * k + 1;
+                if (j < _items.Count() - 1 && selector(j).CompareTo(selector(j + 1)) < 0)
+                    j++;
+                if (selector(k).CompareTo(selector(j)) > 0)
+                    break;
+
+                (_items[k], _items[j]) = (_items[j], _items[k]);
+                k = j;
+            }
+        }
+
+        public TSource DeleteMax(Func<int, TSource> selector)
+        {
+            TSource item = _items[0];
+            (_items[0], _items[_items.Count() - 1]) = (_items[_items.Count() - 1], _items[0]);
+            _items.RemoveAt(_items.Count() - 1);
+            Sink(0, selector);
+            return item;
+        }
+    }
+
+
+    /// <summary>
+    /// How to use: 
+    /// 
+    /// var values = new char[] {'S', 'O', 'R', 'T', 'E', 'X','A', 'M', 'P', 'L', 'E'};
+    /// var source = values.ToList();
+    /// var heap = MakeHeap<char>.Make(source);
+    /// var sortedList = HeapSort<char>.Sort(heap);
+    /// 
+    /// </summary>
+    /// <typeparam name="TSource"></typeparam>
+    public static class MakeHeap<TSource> where TSource : IComparable<TSource>
+    {
+        public static BinaryHeap<TSource> Make(IList<TSource> sources)
+        {
+            BinaryHeap<TSource> binaryHeap = new BinaryHeap<TSource>(sources);
+
+            for (int i = sources.Count() / 2 - 1; i >= 0 ; i--)
+                binaryHeap.Sink(i, i => binaryHeap._items[i]);
+
+            return binaryHeap;
+        }
+    }
+    public static class HeapSort<TSource> where TSource : IComparable<TSource>
+    {
+        public static IList<TSource> Sort(BinaryHeap<TSource> binaryHeap)
+        {
+            var result = new TSource[binaryHeap._items.Count()];
+
+            for (int i = binaryHeap._items.Count() - 1; i >=0 ; i--)
+            {
+                result[i] = binaryHeap.DeleteMax(i => binaryHeap._items[i]);
+            }
+
+            return result;
         }
     }
 }
--- BinaryHeap/Src/Program.cs
+++ BinaryHeap/Src/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Src
 {
@@ -6,7 +7,12 @@ namespace Src
     {
         static void Main(string[] args)
         {
-            
+            var values = new char[] {
+                'S', 'O', 'R', 'T', 'E', 'X','A', 'M', 'P', 'L', 'E'
+            };
+            var source = values.ToList();
+            var heap = MakeHeap<char>.Make(source);
+            var result = HeapSort<char>.Sort(heap);
         }
     }
 }
