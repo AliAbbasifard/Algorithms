daf0165e2d1792ce04adc765af9ed858882682bf
Abbasifard <aliabbasifard@hotmail.com>
2022-09-13 19:34:47 +0430

    Add SymbolTable Interface

---
+++ Symbol Table/Src/ISymbolTable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Src
+{
+    public interface ISymbolTable<TKey, TValue>
+    {
+        void Put(TKey key, TValue value);
+        TValue Get(TKey key);
+        void Delete(TKey key);
+        bool Contains(TKey key);
+        bool isEmpty();
+        int size();
+        IEnumerable<TKey> Keys();
+    }
+}
